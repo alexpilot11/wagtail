4585c82f037d2c82ce2dfd2fda8682752261a777
Alex <alexpilot11@users.noreply.github.com>
2016-01-25 14:07:57 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     author_email='matthew.westcott@torchbox.com',
     url='http://wagtail.io/',
     packages=find_packages(),
-    package_data={'': ['*']},
+    package_data={'': ['*.json']},
     include_package_data=True,
     license='BSD',
     long_description=open('README.rst').read(),
